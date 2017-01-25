1c6bf1a98c29784a98990d15cc3f064736d9c6ea
Pablo <wolter999@gmail.com>
2017-01-25 17:53:07 +0100

    taki tam komit

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,68 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 /**
  * Created by Tomasz on 2017-01-25.
  */
+
+
 public class Main {
 
-    public void showMenu()
-    {
+    static int opcjaMenu = 0;
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+
+        Kino kino;
+
+
+        //kino.wyswietl_wszystkie_miejsca();
+        showMenu();
+        wczytaj();
+
+        while(opcjaMenu != 7)
+        {
+            switch(opcjaMenu)
+            {
+                case 1:
+//                    kino.dodaj_rezerwacje();
+                    System.out.println("opcja 1");
+                    break;
+                case 2:
+//                    kino.anuluj_rezerwacje();
+                    System.out.println("opcja 2");
+                    break;
+                case 3:
+//                    kino.sprawdz_miejsce();
+                    break;
+                case 4:
+//                    kino.wyswietl_wszystkie_miejsca();
+//                    wczytaj();
+                    break;
+                case 5:
+//                    kino.wyswietl_wolne_miejsca();
+//                    wczytaj();
+                    break;
+                case 6:
+//                    kino.wyswietl_zarezerwowane_miejsca();
+//                    wczytaj();
+                    break;
+            }
+
+            showMenu();
+            wczytaj();
+        }
+    }
+
+    private static void wczytaj() throws IOException {
+        System.out.println("Podaj opcje:");
+        opcjaMenu = scanner.nextInt();
+        
+
+    }
+
+
+    private static void showMenu() {
         System.out.println("System rezerwacji miejsc");
         System.out.println("Wybierz opcje:");
         System.out.println("1 - Dodaj rezerwacje");
@@ -16,14 +72,6 @@ public class Main {
         System.out.println("5 - Wyswietl wolne miejsca");
         System.out.println("6 - Wyswietl zarezerwowane miejsca");
         System.out.println("7 - Zamknij system");
-    }
-
-    public void wczytajOpcje(){
-
-    }
-
-    public static void main(String[] args) {
-
     }
 
 }
