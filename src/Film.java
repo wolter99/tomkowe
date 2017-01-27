969ee2d2dea44c86d607804b7c81b6f39eee29c2
Pablo <wolter999@gmail.com>
2017-01-27 17:45:40 +0100

    przerobienie Kino na Film, poczatek prac nad nowa klasa Kino

--- src/Film.java
+++ src/Film.java
@@ -1,27 +1,102 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * Created by Tomasz on 2017-01-25.
  */
 public class Film implements KinoInterface{
+    private static int liczbaMiejsc = 10;
+    private int wybraneMiejsce;
+    private String tytul;
+    private ArrayList<Miejsce> miejsca = new ArrayList<>();
+    Scanner scanner = new Scanner(System.in);
 
+    public Film() {
+        this.tytul = "";
+        this.wybraneMiejsce = 0;
+        for (int i = 0 ; i < liczbaMiejsc ; i++){
+            miejsca.add(new Miejsce("","",i+1,false));
+        }
+    }
 
-
-    @Override
-    public void wypisz() {
-
+    public Film(String tytul) {
+        this.tytul = tytul;
+        this.wybraneMiejsce = 0;
+        for (int i = 0 ; i < liczbaMiejsc ; i++){
+            miejsca.add(new Miejsce("","",i+1,false));
+        }
     }
 
     @Override
-    public void wczytaj() {
+    public void wypisz() { //wypisze wszystkie mejsca
+        for(Miejsce m:miejsca){
+            m.wypisz();
+        }
+    }
 
+    public void wypiszWolneMiejsca(){
+        System.out.println("Wolne miejsca prosze wybrac jedno:");
+        for(Miejsce m:miejsca){
+            if (!m.czyZarezerwowane()){
+                System.out.print(m.numer+", ");
+            }
+        }
+    }
+
+    public void wypiszZarezerwowaneMiejsca(){
+        System.out.println("Zarezerwowane miejsca prosze wybrac jedno aby anulowac:");
+        for(Miejsce m:miejsca){
+            if (m.czyZarezerwowane()){
+                System.out.print(m.numer+", ");
+            }
+        }
+    }
+
+    @Override
+    public void zarezerwuj(){
+        wypiszWolneMiejsca();
+        wybierzMiejsce();
+        if(wybraneMiejsce>0 && wybraneMiejsce<=liczbaMiejsc){
+            miejsca.get(wybraneMiejsce-1).zarezerwuj();
+        }
     }
 
     @Override
     public void wycofajRezewacje() {
+        wypiszZarezerwowaneMiejsca();
+        wybierzMiejsce();
+        if(wybraneMiejsce>0 && wybraneMiejsce<=liczbaMiejsc){
+            for(Miejsce m:miejsca){
+                if (m.numer==wybraneMiejsce){
+                    m.wycofajRezewacje();
+                    break;
+                }
+            }
+        }
+    }
+
+    public void wybierzMiejsce(){
+        wczytaj();
+
+        if(wybraneMiejsce<=0 || wybraneMiejsce>liczbaMiejsc){
+            System.out.println("wybrales liczbe z poza zakresu");
+            wybraneMiejsce = 0;
+        }
 
     }
 
-    @Override
-    public void zarezerwuj() {
+    public void sprawdzMiejsce(){
+        wybierzMiejsce();
+        if(wybraneMiejsce>0 && wybraneMiejsce<=liczbaMiejsc){
+            miejsca.get(wybraneMiejsce - 1).wypisz();
+        }
+        wybraneMiejsce=0;
+    }
 
+    @Override
+    public void wczytaj(){
+        System.out.println("Podaj miejsce:");
+        wybraneMiejsce = scanner.nextInt();
+        //for (int i = 0; i < 20; ++i) System.out.println();
     }
 }
--- src/Kino.java
+++ src/Kino.java
@@ -7,88 +7,61 @@ import java.util.Scanner;
 
 
 public class Kino implements KinoInterface {
-    private static int liczbaMiejsc = 10;
-    private int wybraneMiejsce;
-    private ArrayList<Miejsce> miejsca = new ArrayList<>();
-    Scanner scanner = new Scanner(System.in);
+
+    private static int iloscFilmow = 5; //max filmow w kinie
+    private int wybranyFilm;
+    private ArrayList<Film> filmy = new ArrayList<>();
+    private int liczbaDodanychFilmow; //aktualna liczba filmow w kinie
 
     public Kino() {
-        this.wybraneMiejsce = 0;
-        for (int i = 0 ; i < liczbaMiejsc ; i++){
-            miejsca.add(new Miejsce("","",i+1,false));
-        }
+        this.wybranyFilm = 0;
+        this.liczbaDodanychFilmow = 0;
+    }
+
+    public void wypiszWolneMiejsca(){ //wypisuje wolne miejsca miejsca na kazdy film
+
+    }
+
+    public void wypiszZarezerwowaneMiejsca(){
+
     }
 
     @Override
-    public void wypisz() { //wypisze wszystkie mejsca
-        for(Miejsce m:miejsca){
-                m.wypisz();
-        }
-    }
+    public void wypisz() { //wypisuje wszystkie miejsca miejsca na kazdy film
 
-    private void wypiszWolneMiejsca(){
-        System.out.println("Wolne miejsca prosze wybrac jedno:");
-        for(Miejsce m:miejsca){
-            if (!m.czyZarezerwowane()){
-                System.out.print(m.numer+", ");
-            }
-        }
-    }
-
-    private void wypiszZarezerwowaneMiejsca(){
-        System.out.println("Zarezerwowane miejsca prosze wybrac jedno aby anulowac:");
-        for(Miejsce m:miejsca){
-            if (m.czyZarezerwowane()){
-                System.out.print(m.numer+", ");
-            }
-        }
     }
 
     @Override
     public void zarezerwuj(){
-        wypiszWolneMiejsca();
-        wybierzMiejsce();
-        if(wybraneMiejsce>0 && wybraneMiejsce<=liczbaMiejsc){
-            miejsca.get(wybraneMiejsce-1).zarezerwuj();
-        }
+
     }
 
     @Override
     public void wycofajRezewacje() {
-        wypiszZarezerwowaneMiejsca();
-        wybierzMiejsce();
-        if(wybraneMiejsce>0 && wybraneMiejsce<=liczbaMiejsc){
-            for(Miejsce m:miejsca){
-                if (m.numer==wybraneMiejsce){
-                    m.wycofajRezewacje();
-                    break;
-                }
-            }
-        }
-    }
-
-    public void wybierzMiejsce(){
-        wczytaj();
-
-        if(wybraneMiejsce<=0 || wybraneMiejsce>liczbaMiejsc){
-            System.out.println("wybrales liczbe z poza zakresu");
-            wybraneMiejsce = 0;
-        }
 
     }
+
 
     public void sprawdzMiejsce(){
-        wybierzMiejsce();
-        if(wybraneMiejsce>0 && wybraneMiejsce<=liczbaMiejsc){
-            miejsca.get(wybraneMiejsce - 1).wypisz();
-        }
-        wybraneMiejsce=0;
+
+    }
+
+    public void dodajFilm(){
+
+    }
+
+    public void wybierzFilm(){
+
+    }
+
+    public void wyswietlFilmy(){
+        
     }
 
     @Override
     public void wczytaj(){
-        System.out.println("Podaj miejsce:");
-        wybraneMiejsce = scanner.nextInt();
+        //System.out.println("Podaj miejsce:");
+        //wybraneMiejsce = scanner.nextInt();
         //for (int i = 0; i < 20; ++i) System.out.println();
     }
 }
